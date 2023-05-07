a25ac74321359b82f8c3d1bfe4bb6eda06933b2c
David Beshensky <beshenskydave@gmail.com>
2023-05-07 18:36:17 -0500

    added conditional rendering and title editing

--- app/components/CRUDcomponents/GetTasks.tsx
+++ app/components/CRUDcomponents/GetTasks.tsx
@@ -13,6 +13,8 @@ const GetTasks = () => {
     [id: number]: boolean;
   }>({});
   const [isEditing, setIsEditing] = useState<{ [id: number]: boolean }>({});
+  const [newTitle, setNewTitle] = useState<string>("");
+
 
   const makeApiCall = async () => {
     try {
@@ -37,11 +39,11 @@ const GetTasks = () => {
 
   //HANDLE CHECKBOX CHANGE
   const handleCheckboxChange = async (id: number, checked: boolean) => {
-    
     setCompletionStatus((prevCompletionStatus) => ({
     ...prevCompletionStatus,
     [id]: checked,
     }));
+
     try {
       const response = await fetch(`/api/updateTaskCompletion/${id}`, {
         method: "PUT",
@@ -50,6 +52,7 @@ const GetTasks = () => {
         },
         body: JSON.stringify({ completion_status: checked }),
       });
+
       const updatedTask = await response.json();
       setTasks((prevTasks) =>
       prevTasks.map((task) => {
@@ -68,7 +71,34 @@ const GetTasks = () => {
     }
   };
   //HANDLE TITLE CHANGE
-
+  const handleTitleChange = async (e: React.FormEvent<HTMLFormElement>, id: number, newTitle: string) => {
+    try {
+      const response = await fetch(`/api/updateTaskTitle/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: newTitle }),
+      });
+      const updatedTask = await response.json();
+      setTasks((prevTasks) =>
+        prevTasks.map((task) => {
+          if (task.id === updatedTask.id) {
+            return updatedTask;
+          } else {
+            return task;
+          }
+        })
+      );
+      setIsEditing((prevIsEditing) => ({
+        ...prevIsEditing,
+        [id]: false,
+      }));
+      setNewTitle("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   //HANDLE DELETE TASK
   const handleDeleteTask = async (id: number) => {
     try {
@@ -98,42 +128,67 @@ const GetTasks = () => {
         {tasks.map((task) => (
           <li key={task.id}>
             <div className="w-full border-2 border-gray-200 p-4 m-2">
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  className="form-checkbox h-5 w-5"
-                  checked={completionStatus[task.id] ?? false}
-                  onChange={(e) =>
-                    handleCheckboxChange(task.id, e.target.checked)
-                  }
-                />
-
-                <span className="ml-2 text-white">{task.title}</span>
-
-                <button
-                  onClick={() => handleDeleteTask(task.id)}
-                  className="ml-2 text-red-500"
+              {isEditing[task.id] ? (
+                <form
+                  onSubmit={(e) => handleTitleChange(e, task.id, newTitle)}
+                  className="flex items-center"
                 >
-                  ~Delete~
-                </button>
-                <button
-                  onClick={() =>
-                    setIsEditing((prevIsEditing) => ({
-                      ...prevIsEditing,
-                      [task.id]: true,
-                    }))
-                  }
-                  className="ml-2 text-green-500"
-                >
-                  Edit Title
-                </button>
-              </label>
+                  <input
+                    type="text"
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                    className="ml-2 text-black w-full"
+                    style={{ fontSize: "16px" }}
+                  />
+                  <button
+                    type="submit"
+                    className="ml-2 text-green-500"
+                  >
+                    Submit
+                  </button>
+                </form>
+              ) : (
+                <label className="flex items-center">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox h-5 w-5"
+                    checked={completionStatus[task.id] ?? false}
+                    onChange={(e) =>
+                      handleCheckboxChange(task.id, e.target.checked)
+                    }
+                  />
+                  <span
+                    className="ml-2 text-white"
+                    style={{ fontSize: "16px", minWidth: "50%" }}
+                  >
+                    {task.title}
+                  </span>
+                  <button
+                    onClick={() =>
+                      setIsEditing((prevIsEditing) => ({
+                        ...prevIsEditing,
+                        [task.id]: true,
+                      }))
+                    }
+                    className="ml-2 text-green-500"
+                  >
+                    Edit Title
+                  </button>
+                  <button
+                    onClick={() => handleDeleteTask(task.id)}
+                    className="ml-2 text-red-500"
+                  >
+                    Delete
+                  </button>
+                </label>
+              )}
             </div>
           </li>
         ))}
       </ul>
     </div>
   );
-};
+}
+            
 
 export default GetTasks;
