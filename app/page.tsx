dce9f80a8ca6433d196f6284ec3cf98ce4218f75
David Beshensky <beshenskydave@gmail.com>
2023-05-10 10:31:10 -0500

    refactored the taskList and tasks parent components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Footer from './components/Footer'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import TasksParent from "./components/TasksParent";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Footer />
+      <TasksParent />
     </main>
-  )
+  );
 }
